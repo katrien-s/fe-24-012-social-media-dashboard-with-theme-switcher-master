f87d64358484d4f31ed902659315d984132673cc
Katrien S <ontwerp@katriens.be>
2024-10-10 15:04:41 +0200

    mobile view & basic js-functionality

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const checkbox = document.getElementById('theme-toggle');
+
+function toggleTheme() {
+	checkbox.checked ? setTheme('dark') : setTheme('light');
+}
+
 function setTheme(themeName) {
 	document
 		.getElementById('theme-link')
 		.setAttribute('href', `theme-${themeName}.css`);
 }
+
+// Setting theme preference in localStorage
+
+function savingThemePreference(themeName) {
+	localStorage.setItem('theme', themeName);
+}
+
+window.onload = function () {
+	const savedTheme = localStorage.getItem('theme') || 'light';
+
+	setTheme(savedTheme);
+	checkbox.checked = savedTheme === 'dark';
+};
